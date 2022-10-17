9d6ed6873b22d50a7eca8bdefc0586b8c1968f83
Kototap <kotonesmz31@gmail.com>
2022-10-17 15:44:36 +0000

    [Finish]Unit3

--- leet.rb
+++ leet.rb
@@ -1,54 +1,54 @@
-# input_line = gets
-# strings = input_line.to_s.split('')
+input_line = gets
+strings = input_line.to_s.split('')
 
-# convert = []
+convert = []
 
-# strings.each do |chara|
-#   case chara
-#   when "A" then
-#     convert.push("4")
-#   when "E" then
-#     convert.push("3")
-#   when "G" then
-#     convert.push("6")
-#   when "I" then
-#     convert.push("1")
-#   when "O" then
-#     convert.push("0")
-#   else
-#     convert.push(chara)
-#   end
-# end
-
-# output = convert.join
-
-# puts output
-
-
-def convert_leet(chara)
+strings.each do |chara|
   case chara
   when "A" then
-    "4"
+    convert.push("4")
   when "E" then
-    "3"
+    convert.push("3")
   when "G" then
-    "6"
+    convert.push("6")
   when "I" then
-    "1"
+    convert.push("1")
   when "O" then
-    "0"
-  else # どれにも当てはまらないとき
-    chara
+    convert.push("0")
+  else
+    convert.push(chara)
   end
 end
-  
-input = gets
-strings = input.to_s.split('')
-  
-convert = []
-strings.each do |chara|
-  convert.push(convert_leet(chara))
-end
-  
+
 output = convert.join
+
 puts output
+
+
+# def convert_leet(chara)
+#   case chara
+#   when "A" then
+#     "4"
+#   when "E" then
+#     "3"
+#   when "G" then
+#     "6"
+#   when "I" then
+#     "1"
+#   when "O" then
+#     "0"
+#   else # どれにも当てはまらないとき
+#     chara
+#   end
+# end
+  
+# input = gets
+# strings = input.to_s.split('')
+  
+# convert = []
+# strings.each do |chara|
+#   convert.push(convert_leet(chara))
+# end
+  
+# output = convert.join
+# puts output
